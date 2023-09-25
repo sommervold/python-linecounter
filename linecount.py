489a80193d2e288514705eb796d73d0f64459c8f
sommervold <nicolai@sommervold.net>
2023-09-25 19:42:52 +0200

    Add: base line counter

---
+++ linecount.py
@@ -0,0 +1,25 @@
+import os
+import sys
+
+def count_lines(filepath: str):
+    """Returns number of newline characters in a file"""
+    with open(filepath, "r") as f:
+        return f.read().count("\n")
+
+def has_extension(filepath: str, extensions: list):
+    for extension in extensions:
+        if filepath.endswith(extension):
+            return True
+    return False
+
+def main(path: str, extensions: list):
+    count = 0
+    for path, _, files in os.walk(path):
+        for filepath in files:
+            if has_extension(filepath, extensions):
+                count += count_lines(os.path.join(path, filepath))
+    return count
+
+
+if __name__ == "__main__":
+    print(main(*sys.argv[1], sys.argv[2:]))
