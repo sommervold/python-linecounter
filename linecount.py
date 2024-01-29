c4acdc0599669b70cf95ca1b959dbf03f1d98bbf
Nicolai Sommervold <nicolai@sommervold.net>
2024-01-29 22:08:30 +0100

    style: black formatting

--- linecount.py
+++ linecount.py
@@ -2,6 +2,7 @@ import os
 import sys
 import argparse
 
+
 def count_lines(filepath: str):
     """Returns number of newline characters in a file"""
     try:
@@ -11,6 +12,7 @@ def count_lines(filepath: str):
         # Likely a binary file, skip.
         return 0
 
+
 def has_extension(filepath: str, extensions: list[str]):
     """Returns True if filepath has any of the extensions in the
     extensions list"""
@@ -18,6 +20,7 @@ def has_extension(filepath: str, extensions: list[str]):
         if filepath.endswith(extension):
             return True
     return False
+
 
 def main(path: str, extensions: list[str], ignore: list[str]):
     """Count number of lines in all the files in a directory that has
@@ -29,6 +32,7 @@ def main(path: str, extensions: list[str], ignore: list[str]):
                 count += count_lines(os.path.join(path, filepath))
     return count
 
+
 class _LinecountNamespace(argparse.Namespace):
     path: str
     exclude: list[str]
@@ -37,11 +41,25 @@ class _LinecountNamespace(argparse.Namespace):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-p", "--path", default=".", )
-    parser.add_argument("-i", "--ignore", action="append", default=[], help="Files and directories with this name will be ignored.")
-    parser.add_argument("-e", "--extension", action="append", default=[],
-                         help="Files with this extension will be included in the count.")
-    
+    parser.add_argument(
+        "-p",
+        "--path",
+        default=".",
+    )
+    parser.add_argument(
+        "-i",
+        "--ignore",
+        action="append",
+        default=[],
+        help="Files and directories with this name will be ignored.",
+    )
+    parser.add_argument(
+        "-e",
+        "--extension",
+        action="append",
+        default=[],
+        help="Files with this extension will be included in the count.",
+    )
+
     args: _LinecountNamespace = parser.parse_args()
     result = main(args.path, args.extensions, args.exclude)
-
